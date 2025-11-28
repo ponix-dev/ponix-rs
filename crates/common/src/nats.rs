e118b0103b972ea9e5b1b052da6242fb25af8bf9
rallinator7 <rallinator7@gmail.com>
2025-11-28 12:51:44 -0600

    refactor: removes old nats consuming implementation

--- crates/common/src/nats.rs
+++ crates/common/src/nats.rs
@@ -1,15 +1,11 @@
 mod client;
-mod consumer;
 mod middleware;
-mod protobuf;
 mod tower_consumer;
 mod trace_context;
 mod traits;
 
 pub use client::*;
-pub use consumer::*;
 pub use middleware::*;
-pub use protobuf::*;
 pub use tower_consumer::*;
 pub use trace_context::*;
 pub use traits::*;
