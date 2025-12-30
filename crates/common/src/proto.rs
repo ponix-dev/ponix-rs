57f0292c1f071e92a624099f8f02aee595d9074c
Simon Rall <simonrall47@gmail.com>
2025-12-30 09:23:46 -0600

    feat: adds workspace service

--- crates/common/src/proto.rs
+++ crates/common/src/proto.rs
@@ -3,9 +3,11 @@ mod envelope;
 mod gateway;
 mod organization;
 mod user;
+mod workspace;
 
 pub use device::*;
 pub use envelope::*;
 pub use gateway::*;
 pub use organization::*;
 pub use user::*;
+pub use workspace::*;
